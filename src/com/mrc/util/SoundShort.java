baba62b9adf129177ab33e723c9cdb85fd99ef80
MRC01 <mrc@mclements.net>
2019-10-20 14:13:41 -0700

    Update SoundShort.java

--- src/com/mrc/util/SoundShort.java
+++ src/com/mrc/util/SoundShort.java
@@ -1,6 +1,5 @@
 /* Copyright 1993, 2012 by Michael R. Clements
  * This software is open source and free to distribute and create derivative works.
- * But this notice of copyright and author must be retained.
 */
 package com.mrc.util;
 
